bf39716a9af9327bf42da12a0b135c8cf13e4a93
Anthony Dombrowski <akdflyinvee73@gmail.com>
2024-04-12 10:24:07 -0500

    client submit i don't remember

--- src/actions/survey/clientSubmit.ts
+++ src/actions/survey/clientSubmit.ts
@@ -8,6 +8,7 @@ export default async function ClientSubmit(formData: FormData) {
   const purify = DOMPurify(window);
   const data = [];
   const dataEntries = formData.entries();
+
   for (const [k, v] of dataEntries) {
     const kClean = purify.sanitize(v + "");
     const vClean = purify.sanitize(v + "");
